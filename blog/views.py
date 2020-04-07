462878d0d41f52e825581ea65d4162a89ae17d58
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2020-04-07 21:07:24 +0530

    Blog file change

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'  
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
@@ -28,7 +28,7 @@ class PostListView(ListView):
 
 class UserPostListView(ListView):
     model = Post
-    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html' 
     context_object_name = 'posts'
     paginate_by = 5
 
